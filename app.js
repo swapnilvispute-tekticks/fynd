33530ff46419355008742c2a0cc5b4ab0d268e89
Swapnil <swapnildhvispute@gmail.com>
2020-08-14 10:46:31 +0530

    cors changes

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var cors = require('cors')
+var app = express()
+app.use(cors())
 var indexRouter = require('./routes/index')
 
-var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-
+/*
 // cors
 var whitelist = [
   'http://localhost:4200',
@@ -28,6 +29,7 @@ var corsOptionsDelegate = function (req, callback) {
   callback(null, corsOptions) // callback expects two parameters: error and options
 }
 app.use(cors(corsOptionsDelegate))
+*/
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
